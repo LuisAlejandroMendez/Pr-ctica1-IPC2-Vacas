6b714c1fd44dd119c8216bd63f1de7d9a466fdbf
Luis Alejandro <lamr467@gmail.com>
2021-12-09 22:10:59 -0600

    Se subio la practica completada

---
+++ js/login.js
@@ -0,0 +1,9 @@
+let nombre=document.getElementById("user");
+let pass=document.getElementById("password");
+
+//Impresiones en consola como pidió el Inge
+function login(){
+    console.log(nombre.value);
+    console.log(pass.value);
+    alert(nombre.value);
+}
